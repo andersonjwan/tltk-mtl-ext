d390c40533b470779003780cf6e93d2dc38f022c
andersonjwan <andersonjwan@outlook.com>
2020-11-16 22:10:04 -0700

    added negation rule unit tests

---
+++ tests/testNegationRule.py
@@ -0,0 +1,32 @@
+import tltk_mtl as MTL
+import tltk_mtl_ext as MTLE
+import antlr4
+
+from STQLLexer import STQLLexer as Lexer
+from STQLParser import STQLParser as Parser
+from STQLVisitor import STQLVisitor as Visitor
+
+def testNegationRule():
+	preds = {}
+	preds['pred1'] = MTL.Predicate('pred1', 1, 2)
+	preds['pred2'] = MTL.Predicate('pred2', 2, 4)
+	preds['pred3'] = MTL.Predicate('pred3', 4, 8)
+	
+	assert isinstance(MTLE.parse_mtl('!pred1', preds), MTL.Not), printFail(1)
+	printPass(1)
+	assert isinstance(MTLE.parse_mtl('!!(pred2)', preds), MTL.Not), printFail(2)
+	printPass(2)
+	assert isinstance(MTLE.parse_mtl('!pred3', preds), MTL.Not), printFail(3)
+	printPass(3)
+	assert isinstance(MTLE.parse_mtl('!(pred1 and pred2)', preds), MTL.Not), printFail(4)
+	printPass(4)
+	assert isinstance(MTLE.parse_mtl('!!!!!(pred1 and pred2 and pred3)', preds), MTL.Not), printFail(5)
+	printPass(5)
+
+def printPass(test_num):
+    print('[Negation Test] Assertion No. %02d PASSED.' % (test_num))
+
+def printFail(test_num):
+    return '[Negation Test] Assertion No. %02d FAILED' % (test_num)
+
+testNegationRule()
